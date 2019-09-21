ef72ed2896e5604d493b7bd8b3a9da539570194a
kevinlandun@gmail.com <1qaz2wsx3edcQA1@>
2019-09-21 11:55:07 +0800

    初始提交

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # luffy_stark
+
+```bash
+pip freeze > requirements.txt 环境安装导出模块
+项项目环境  安装
+pip install -r requirements.txt
+```
--- luffy_startk/settings.py
+++ luffy_startk/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app01.apps.App01Config',
+    'app02.apps.App02Config',
 ]
 
 MIDDLEWARE = [
